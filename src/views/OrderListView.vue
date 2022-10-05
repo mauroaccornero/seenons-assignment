8fd34d7d4c07690073f892719ee904ac305cb933
Mauro Accornero <mauro.accornero@tetrapak.com>
2022-10-05 09:24:18 +0200

    feat: update styles
    - update root var for colors and spacing
    - split css files
    - add card, button, chips, header styles

---
+++ src/views/OrderListView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import OrderItem from "@/views/OrderItem.vue";
+import type IOrderItem from "@/common/interfaces/IOrderItem";
+
+interface Props {
+  orders: IOrderItem[] | null;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <main>
+    <h1>Orders</h1>
+    <div :if="props.orders">
+      <OrderItem
+        v-for="order in props.orders"
+        :key="order.order_id"
+        :order="order"
+      />
+    </div>
+  </main>
+</template>
